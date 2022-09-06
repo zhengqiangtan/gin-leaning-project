a1cd602aa31d619bb459b596acdef0e1755ca93b
zhengqiang <844870676@qq.com>
2022-09-06 15:40:27 +0800

    添加公共工具类

---
+++ Util/CommonUtil.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	fmt.Println(VerifyEmailFormat("12345@126.com")) //true
+	fmt.Println(VerifyEmailFormat("12345126.com"))  //false
+}
+
+func VerifyEmailFormat(email string) bool {
+	pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+}
